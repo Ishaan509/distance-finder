49d22441554ed6b546df933e5d73c646c61a04e4
Ishaan <shindeishaan777@gmail.com>
2026-05-25 07:00:42 +0530

    logic change in finding by range

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -53,8 +53,8 @@ async function apiPost (req,res,next){
     let districtWithDistance = [];
     let latitudeFrom,longitudeFrom,latitudeTo,longitudeTo;
     let latitudeFromRad,longitudeFromRad,latitudeToRad,longitudeToRad;
-    let selState = req.body.payload.stateF;
-    let selDistrict = req.body.payload.districtF;
+    let selState = req.body.stateF;
+    let selDistrict = req.body.districtF;
 
     let allDistExceptOne = await districts_states.find({state:selState, district:{$nin : [selDistrict]}});
     let oneDist = await districts_states.find({state:selState,district : selDistrict});
@@ -63,9 +63,9 @@ async function apiPost (req,res,next){
     longitudeFrom = oneDist[0].lon;
 
     let allDistrictWithDistance=[];
-    if(req.body.payload.sliderFlag == true){
+    if(req.body.sliderFlag == true){
         let data = {
-            ...req.body.payload,
+            ...req.body,
             latitudeFrom,
             longitudeFrom
         };
@@ -112,10 +112,11 @@ function sortFunction(a, b) {
     }
 }
 
-function apiProximity(data){
-
+async function apiProximity(data){
+    let lsDistState = [];
+    let allDistrictWithDistance = [];
     if(data.sliderFlag){
-        let proximityLoc = geoSpatial_district_states.find({
+        let proximityLoc = await geoSpatial_district_states.find({
             location:{
                 $near:{
                     $geometry:{ type:"Point", coordinates:[data.longitudeFrom,data.latitudeFrom]},
@@ -124,12 +125,38 @@ function apiProximity(data){
                 }
             }
         });
+        lsDistState = proximityLoc;
+        
     }
+
+    lsDistState.forEach((distLatLon)=>{
+            latitudeTo = distLatLon.location.coordinates[1];
+            longitudeTo = distLatLon.location.coordinates[0];
+            // degrees to radians.
+            longitudeFromRad =  data.longitudeFrom * Math.PI / 180;
+            longitudeToRad = longitudeTo * Math.PI / 180;
+            latitudeFromRad = data.latitudeFrom * Math.PI / 180;
+            latitudeToRad = latitudeTo * Math.PI / 180;
+            
+            // Haversine formula 
+            let dlon = longitudeToRad - longitudeFromRad; 
+            let dlat = latitudeToRad - latitudeFromRad;
+            let a = Math.pow(Math.sin(dlat / 2), 2)
+                     + Math.cos(latitudeFromRad) * Math.cos(latitudeToRad)
+                     * Math.pow(Math.sin(dlon / 2),2);
+            
+            let c = 2 * Math.asin(Math.sqrt(a));
+            
+            // Radius of earth in kilometers.
+            let r = 6371;
+            
+            let res = Math.round(c * r * 1000)/1000;
+            allDistrictWithDistance.push({district:distLatLon.district, distance:res});
+        });
     
-    proximityLoc.then(val => {
-        console.log(val);
-        return val;
-    });
+        allDistrictWithDistance.sort(sortFunction);
+    
+    return allDistrictWithDistance;
 }
 
 module.exports = {apiGet , apiPost, apiMaxDistance};
